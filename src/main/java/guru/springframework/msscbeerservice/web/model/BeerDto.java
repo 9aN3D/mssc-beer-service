14ce6c40a6d2c59ca94c6eb00ec92fdd96aecf40
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2019-11-01 11:37:10 +0100

    Add beer models

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,36 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDto {
+
+    private UUID id;
+
+    private Integer version;
+
+    private OffsetDateTime createTime;
+
+    private OffsetDateTime lastModifiedTime;
+
+    private String beerName;
+
+    private BeerStyle beerStyle;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,7 @@
+package guru.springframework.msscbeerservice.web.model;
+
+public enum BeerStyle {
+
+    SAISON, PILSNER, LAGER, GOSE, BOCK, PORTER, STOUT, ALE, BITTER, DUNKEL
+
+}
