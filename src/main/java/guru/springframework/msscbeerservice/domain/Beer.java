7883cc6038f448e3cbcff8f2c0e2ec9f9a97f2f2
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2019-11-01 18:31:47 +0100

    Add beer entity

---
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -0,0 +1,64 @@
+package guru.springframework.msscbeerservice.domain;
+
+import guru.springframework.msscbeerservice.web.model.BeerStyle;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id", "version", "createTime"})
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Integer version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createTime;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedTime;
+
+    private String beerName;
+
+    private BeerStyle beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer minOnHand;
+
+    private Integer quantityToBrew;
+
+}
+
