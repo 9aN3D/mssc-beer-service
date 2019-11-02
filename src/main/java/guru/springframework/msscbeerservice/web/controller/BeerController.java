8c7a3734bb333898ea7788bb4e1bd3d066882205
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2019-11-02 12:16:00 +0100

    Add error handler

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbeerservice.web.controller;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,30 +11,33 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDto> getById(@PathVariable("beerId") UUID id) {
+    public ResponseEntity<BeerDto> getById(@NotNull @PathVariable("beerId") UUID id) {
         //TODO impl
         return new ResponseEntity<>(BeerDto.builder().build(), OK);
     }
 
     @PostMapping
-    public ResponseEntity save(@RequestBody BeerDto beer) {
+    public ResponseEntity save(@Valid @NotNull @RequestBody BeerDto beer) {
         //TODO impl
         return new ResponseEntity<>(CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity update(@PathVariable("beerId") UUID id, @RequestBody BeerDto beer) {
+    public ResponseEntity update(@NotNull @PathVariable("beerId") UUID id, @Valid @NotNull @RequestBody BeerDto beer) {
         //TODO impl
         return new ResponseEntity<>(NO_CONTENT);
     }
